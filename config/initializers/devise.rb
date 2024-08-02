0ac6fc270da714120f332150ea2cbe38e6b92b38
mtm-YuzanaKyaw <mtm.yuzanakyaw@gmail.com>
2024-08-02 16:43:50 +0630

    [remove] remaining secret keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -305,8 +305,7 @@ Devise.setup do |config|
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
 
-  google_client_id = '518703441732-am4hmshlbv2kgpvlkcn4vqpje7uan8vv.apps.googleusercontent.com'
-  google_client_secret = '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'
+
 
   config.omniauth :google_oauth2, google_client_id, google_client_secret,
   {
